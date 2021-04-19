0ffe1021bb3a80fa0ad2dbd598f5c34de640683b
Beastman9095 <ugurkurtulus06@gmail.com>
2021-04-19 23:25:11 +0300

    Add project files.

---
+++ Ham5teakPresence/CMessageB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ham5teakPresence
+{
+    public partial class CMessageB : Form
+    {
+
+        public CMessageB()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CMessageB_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
