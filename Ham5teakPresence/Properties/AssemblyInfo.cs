e69d93a20d52ccaec01eec3a4a352c0ff494b8a4
Beastman9095 <ugurkurtulus06@gmail.com>
2021-04-20 12:05:16 +0300

    Updated AssemblyInfo.cs

--- Ham5teakPresence/Properties/AssemblyInfo.cs
+++ Ham5teakPresence/Properties/AssemblyInfo.cs
@@ -24,11 +24,10 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// Version information for an assembly consists of the following four values:
+// Major Version
+// Minor Version
+// Build Number
+// Revision
+[assembly: AssemblyVersion("1.0.7.0")]  
+[assembly: AssemblyFileVersion("1.0.7.0")]
